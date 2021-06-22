e5902e6978fa7c95a2ef24c43d54628b51a2a6d5
Jerome Ching <nathan_9_9@hotmail.com>
2021-06-22 12:08:17 -0400

    Added Remote Host

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
-export const HostName = "LocalHost";
+export const RemoteURI = "mongodb+srv://admin:admin@cluster-midterm.4otym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
